0e2f89554291f166041b8a63098f155b46ded0db
Joshua Joseph <60847326+joshuajoseph-stu@users.noreply.github.com>
2022-03-23 14:59:19 +0530

    fix

--- JewelleryStoreProject/src/main/java/com/pixeltrice/springbootQRcodegeneratorapp/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pixeltrice.springbootQRcodegeneratorapp;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long>{}
